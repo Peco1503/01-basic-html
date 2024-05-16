d07808f9531403a0793721ebf39676ac3fdc53c2
Peco1503 <102172862+Peco1503@users.noreply.github.com>
2024-05-16 11:30:01 -0600

    bye bye

--- 04-nextjs-rick-and-morty/src/const/paths.ts
+++
@@ -1,5 +0,0 @@
-export const paths = {
-  allCharacters: "/all-characters",
-  favoriteCharacters: "/favorite-characters",
-  character: "/character",
-};
--- 04-nextjs-rick-and-morty/src/types/Episode.ts
+++
@@ -1,9 +0,0 @@
-export interface Episode {
-  id: string;
-  name: string;
-  air_date: string;
-  episode: string;
-  characters: string[];
-  url: string;
-  created: string;
-}
